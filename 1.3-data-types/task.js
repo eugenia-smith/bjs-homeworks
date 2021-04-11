020bbeaf4b196b168c3c36446a1c2d70f6a857b2
eugenia-smith <60076849+eugenia-smith@users.noreply.github.com>
2021-04-11 23:51:48 +0300

    Начато решение задачи 1

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,6 +1,22 @@
+"use strict";
+
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+
+    
+  
+if (typeof percent === "undefined" || percent <= 0) {
+return `Параметр "Процентная ставка" содержит неправильное значение ${percent}`;
+} else if (typeof contribution === "undefined" || contribution <= 0) {
+    return `Параметр "Сумма первоначального взноса" содержит неправильное значение ${contribution}`;
+} else if (typeof amount === "undefined" || amount <= 0) {
+    return `Параметр "Сумма кредита" содержит неправильное значение ${amount}`;
+}
+
+
+
+
+    
+    return totalAmount;
 }
 
 function getGreeting(name) {
