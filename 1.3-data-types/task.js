51fa2940098aeb24ae0d5c93839ca36df2589c4d
eugenia-smith <60076849+eugenia-smith@users.noreply.github.com>
2021-04-18 22:37:42 +0300

    Написано какое-то сырое решение

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,25 +1,29 @@
 "use strict";
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
-let percentSum = Number(percent);
+let percentSum = Number(percent) / 12;
 let contributionSum = Number(contribution);
 let amountSum = Number(amount);
-let currentDate = Date();
+let currentDate = new Date();
 let currentDateInSec = currentDate.getTime();
 let dateToPay = date.getTime();
-let creditPeriod = (dateToPay - currentDateInSec)/86400;
-    
+let creditPeriod = ((dateToPay - currentDateInSec)/86400000)/30;
+let creditSum = amountSum - contributionSum;
+
+let monthlyPayment = creditSum * (percentSum + percentSum / (((1 + percentSum)^creditPeriod) - 1));
+
+let totalAmount = (monthlyPayment * creditPeriod).toFixed(2);
+
   
 if (typeof percent === "undefined" || percent <= 0) {
 return `Параметр "Процентная ставка" содержит неправильное значение ${percent}`;
-} else if (typeof contribution === "undefined" || contribution <= 0) {
+} else if (typeof contribution === "undefined" || contribution < 0) {
     return `Параметр "Сумма первоначального взноса" содержит неправильное значение ${contribution}`;
 } else if (typeof amount === "undefined" || amount <= 0) {
     return `Параметр "Сумма кредита" содержит неправильное значение ${amount}`;
 }
 
-
-
+console.log(totalAmount);
 
     
     return totalAmount;
