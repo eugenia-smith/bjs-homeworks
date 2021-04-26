b67d595828e644354fb06f50091a19d32cc20882
eugenia-smith <60076849+eugenia-smith@users.noreply.github.com>
2021-04-27 00:05:48 +0300

    Написаны обе функции для задачи 1

--- 2.1-functions/task.js
+++ 2.1-functions/task.js
@@ -1,36 +1,48 @@
 'use strict';
 
 //Задача 1
+
+let x1;
+let x2;
+
 function getSolutions (a, b, c) {
 
     let D = Math.pow(b, 2) - 4 * a * c;
     let roots = [];
-    let x1;
-    let x2;
-    let result = {
+    let solutions = {
         D,
         roots
     };
 
-
     if (D < 0) {
-        return result;
+        return solutions;
 
     } else if (D == 0) {
         x1 = -b / (2 * a);
         roots.push(x1);
 
-        return result;
-
     } else {
-        x1 = (-b - Math.sqrt(D)) / (2 * a);
-        x2 = (-b + Math.sqrt(D)) / (2 * a);
+        x1 = (-b + Math.sqrt(D)) / (2 * a);
+        x2 = (-b - Math.sqrt(D)) / (2 * a);
         roots.push(x1, x2);
-
-        return result;
     }
+
+    return solutions;
 }
 
+
 function showSolutionsMessage (a, b, c) {
+    let result = getSolutions(a, b, c);
+
+    console.log(`Вычисляем корни квадратного уравнения ${a}x² + ${b}x + ${c}`);
+    console.log(`Значение дискриминанта: ${result.D}`);
+
+    if (result.roots.length == 2) {
+        console.log(`Уравнение имеет два корня X₁ = ${x1}, X₂ = ${x2}`);
+    } else if (result.roots.length == 1) {
+        console.log(`Уравнение имеет один корень X₁ = ${x1}`);
+    } else {
+        console.log('Уравнение не имеет вещественных корней');
+    }
 
 }
