f06afe6f419f09aeb133a52d8ddcdeb67ea5c8d2
eugenia-smith <ev-genius4@mail.ru>
2021-04-27 14:56:38 +0300

    Правка по замечаниям преподавателя. Задача 1

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,30 +1,41 @@
 "use strict";
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
-let percentSum = Number(percent);
-let percentPerMonth = percentSum / 1200;
-let contributionSum = Number(contribution);
-let amountSum = Number(amount);
-let currentDate = new Date();
-let currentDateInSec = currentDate.getTime();
-let dateToPay = date.getTime();
-let creditPeriod = Math.ceil(((dateToPay - currentDateInSec)/86400000)/30);
-let creditSum = amountSum - contributionSum;
 
-let monthlyPayment = creditSum * (percentPerMonth + percentPerMonth / (((1 + percentPerMonth)^creditPeriod) - 1));
+    console.log({percent, contribution, amount, date});
 
-let totalAmount = (monthlyPayment * creditPeriod).toFixed(2);
+    let percentSum = new Number(percent);
+    let contributionSum = new Number(contribution);
+    let amountSum = new Number(amount);
+    let currentDate = new Date();
+    let dateToPay = date.getTime();
+    let today = currentDate.getTime();
+    //new Date(currentDate.getDay, currentDate.getMonth, currentDate.getFullYear);
 
-  
-if (typeof percent === "undefined" || percent <= 0) {
-return `Параметр "Процентная ставка" содержит неправильное значение ${percent}`;
-} else if (typeof contribution === "undefined" || contribution < 0) {
-    return `Параметр "Сумма первоначального взноса" содержит неправильное значение ${contribution}`;
-} else if (typeof amount === "undefined" || amount <= 0) {
-    return `Параметр "Сумма кредита" содержит неправильное значение ${amount}`;
-}
 
-//console.log(creditPeriod);
+
+
+    if (typeof percent === "undefined" || percent <= 0 || typeof percent === "string") {
+        return `Параметр "Процентная ставка" содержит неправильное значение ${percent}`;
+    } else if (typeof contribution === "undefined" || contribution < 0) {
+        return `Параметр "Сумма первоначального взноса" содержит неправильное значение ${contribution}`;
+    } else if (typeof amount === "undefined" || amount <= 0) {
+        return `Параметр "Сумма кредита" содержит неправильное значение ${amount}`;
+    } else if (typeof dateToPay === "undefined" || dateToPay <= today.getTime()) {
+        return `Параметр "Дата" содержит неправильное значение ${date}`;
+    }
+
+    let percentPerMonth = percentSum / 1200;
+    let currentDateInSec = currentDate.getTime();
+    let creditPeriod = Math.ceil(((dateToPay - currentDateInSec) / 86400000) / 30);
+    let creditSum = amountSum - contributionSum;
+
+    let monthlyPayment = creditSum * (percentPerMonth + percentPerMonth / (((1 + percentPerMonth) ^ creditPeriod) - 1));
+
+    let totalAmount = (monthlyPayment * creditPeriod).toFixed(2);
+
+
+    //console.log(creditPeriod);
 
     return totalAmount;
 }
