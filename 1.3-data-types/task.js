ae035c82da735a2ad653a1315e29e85fefc9fc27
eugenia-smith <ev-genius4@mail.ru>
2021-04-28 13:57:24 +0300

    Исправлено решение задачи 1

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -4,38 +4,27 @@ function calculateTotalMortgage(percent, contribution, amount, date) {
 
     console.log({percent, contribution, amount, date});
 
-    let percentSum = new Number(percent);
-    let contributionSum = new Number(contribution);
-    let amountSum = new Number(amount);
+    let percentSum = parseInt(percent, 10);
+    let contributionSum = parseInt(contribution, 10);
+    let amountSum = parseInt(amount, 10);
     let currentDate = new Date();
     let dateToPay = date.getTime();
-    let today = currentDate.getTime();
-    //new Date(currentDate.getDay, currentDate.getMonth, currentDate.getFullYear);
-
-
-
-
-    if (typeof percent === "undefined" || percent <= 0 || typeof percent === "string") {
+    
+    if (typeof percent === "undefined" || percent <= 0) {
         return `Параметр "Процентная ставка" содержит неправильное значение ${percent}`;
     } else if (typeof contribution === "undefined" || contribution < 0) {
         return `Параметр "Сумма первоначального взноса" содержит неправильное значение ${contribution}`;
     } else if (typeof amount === "undefined" || amount <= 0) {
         return `Параметр "Сумма кредита" содержит неправильное значение ${amount}`;
-    } else if (typeof dateToPay === "undefined" || dateToPay <= today.getTime()) {
+    } else if (typeof dateToPay === "undefined" || dateToPay <= currentDate.getTime()) {
         return `Параметр "Дата" содержит неправильное значение ${date}`;
     }
 
-    let percentPerMonth = percentSum / 1200;
-    let currentDateInSec = currentDate.getTime();
-    let creditPeriod = Math.ceil(((dateToPay - currentDateInSec) / 86400000) / 30);
+    let percentPerMonth = percentSum / 100 / 12;
+    let creditPeriod = Math.round(((dateToPay - currentDate.getTime()) / 86400000) / 30);
     let creditSum = amountSum - contributionSum;
-
-    let monthlyPayment = creditSum * (percentPerMonth + percentPerMonth / (((1 + percentPerMonth) ^ creditPeriod) - 1));
-
+    let monthlyPayment = creditSum * (percentPerMonth + percentPerMonth / (Math.pow((1 + percentPerMonth), creditPeriod) - 1));
     let totalAmount = (monthlyPayment * creditPeriod).toFixed(2);
-
-
-    //console.log(creditPeriod);
 
     return totalAmount;
 }
