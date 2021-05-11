5c129fe8210c58f5435eb5166de195177ab50ead
eugenia-smith <60076849+eugenia-smith@users.noreply.github.com>
2021-05-11 23:08:30 +0300

    Решена задача 1

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,4 +1,10 @@
-//String.prototype.isPalindrome - для задачи №1
+String.prototype.isPalindrome = function () {
+    let original = this.toLowerCase().replace(/\s/g, '');
+    let reverse = original.split('').reverse().join('');
+    //console.log(original);
+    //console.log(reverse);
+    return original === reverse;
+}
 
 function getAverageMark(marks) {
     // код для задачи №2 писать здесь
