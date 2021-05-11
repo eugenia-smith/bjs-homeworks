a9c03c2d3c575af290c8af4dc947dba3ea9bbe52
eugenia-smith <60076849+eugenia-smith@users.noreply.github.com>
2021-05-12 00:17:57 +0300

    Решена задача 2

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -7,8 +7,19 @@ String.prototype.isPalindrome = function () {
 }
 
 function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
+    let sum = 0;
+
+    if (marks.length == 0) {
+        return sum;
+    }
+
+    for (let i = 0; i < marks.length; i++) {
+        sum += marks[i];
+    }
+    let average = sum / marks.length;
+    let roundedAverage = Math.round(average);
+
+    return roundedAverage;
 }
 
 function checkBirthday(birthday) {
